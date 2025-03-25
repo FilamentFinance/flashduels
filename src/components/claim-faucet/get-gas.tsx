072997753ad94613ee8677521f8cbfc01e49cc21
rajeebkm <rkm2541997@gmail.com>
2025-03-25 22:48:45 +0530

    tooltip added for Get Gas

--- src/components/claim-faucet/get-gas.tsx
+++ src/components/claim-faucet/get-gas.tsx
@@ -3,38 +3,65 @@ import { Button } from '@/shadcn/components/ui/button';
 import axios from 'axios';
 import { FC, useState } from 'react';
 import { useAccount } from 'wagmi';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/shadcn/components/ui/tooltip";
 
 const GAS_API_URL = 'https://orderbookv3.filament.finance/gastank';
 
 const GetGas: FC = () => {
-  const { address } = useAccount();
-  const [gasClaimed, setGasClaimed] = useState(false);
 
-  const getGas = async () => {
-    try {
-      await axios.post(`${GAS_API_URL}/api/transfer`, {
-        address: address?.toLowerCase() ?? '',
-        app: 'flash_duels',
-      });
-      setGasClaimed(true);
-      localStorage.setItem('gas_claimed', 'true');
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      console.log('Error', error.message);
-      if (error.message === 'Not Eligible') {
-        localStorage.setItem('gas_claimed', 'true');
-        setGasClaimed(true);
-      }
-    }
-  };
+  // Note - It will be enabled on mainnet soon
+  
+  // const { address } = useAccount();
+  // const [gasClaimed, setGasClaimed] = useState(false);
+
+  // const getGas = async () => {
+  //   try {
+  //     await axios.post(`${GAS_API_URL}/api/transfer`, {
+  //       address: address?.toLowerCase() ?? '',
+  //       app: 'flash_duels',
+  //     });
+  //     setGasClaimed(true);
+  //     localStorage.setItem('gas_claimed', 'true');
+  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //   } catch (error: any) {
+  //     console.log('Error', error.message);
+  //     if (error.message === 'Not Eligible') {
+  //       localStorage.setItem('gas_claimed', 'true');
+  //       setGasClaimed(true);
+  //     }
+  //   }
+  // };
+
+  // return (
+  //   <Button
+  //     className="font-semibold bg-gradient-pink text-black"
+  //     onClick={getGas}
+  //   >
+  //     Get Gas
+  //   </Button>
+  // );
 
   return (
-    <Button
-      className="font-semibold bg-gradient-pink text-black"
-      onClick={getGas}
-    >
-      Get Gas
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            className="font-semibold bg-gradient-pink text-black opacity-50 cursor-not-allowed"
+            disabled={true}
+          >
+            Get Gas
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Will be available on Mainnet soon</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
