81b20db0b5ba2170c6e9052fe54a67c0b4c385e7
rajeebkm <rkm2541997@gmail.com>
2025-04-17 00:54:44 +0530

    Get gas enabled based on network id

--- src/components/claim-faucet/get-gas.tsx
+++ src/components/claim-faucet/get-gas.tsx
@@ -40,7 +40,12 @@ const GetGas: FC = () => {
       setIsLoading(false);
     }
   };
-
+  console.log({ chainId });
+  const chainidsei = sei.id;
+  console.log({ chainidsei });
+  const chainidseitestnet = seiTestnet.id;
+  console.log({ chainidseitestnet });
+  console.log(chainId === sei.id);
   if (chainId === sei.id) {
     return (
       <Button
@@ -98,62 +103,6 @@ const GetGas: FC = () => {
       </Button>
     );
   } else if (chainId === seiTestnet.id) {
-    return (
-      <Button
-        className="font-semibold text-pink-300 border border-pink-300 bg-transparent
-        hover:shadow-lg hover:scale-[1.02] hover:bg-pink-300/10"
-        onClick={getGas}
-        disabled={gasClaimed || isLoading}
-      >
-        <span className="flex items-center gap-1">
-          {gasClaimed ? (
-            '0.02 SEI Claimed 🎉'
-          ) : isLoading ? (
-            <>
-              <svg
-                className="animate-spin -ml-1 mr-2 h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                />
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                />
-              </svg>
-              Claiming...
-            </>
-          ) : (
-            <>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M13 10V3L4 14h7v7l9-11h-7z"
-                />
-              </svg>
-              Get Gas
-            </>
-          )}
-        </span>
-      </Button>
-    );
-  } else {
     return (
       <Button
         className="font-semibold bg-gradient-pink text-black opacity-50 cursor-not-allowed
