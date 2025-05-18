da30016d41b04081170256d5f559990d26036265
rajeebkm <rkm2541997@gmail.com>
2025-05-18 13:48:37 +0530

    twitter profile added

--- next.config.js
+++ next.config.js
@@ -2,23 +2,25 @@
 const nextConfig = {
   images: {
     domains: [
-      'flashduel-images.s3.us-east-1.amazonaws.com'  // Add your S3 bucket domain
+      'flashduel-images.s3.us-east-1.amazonaws.com',  // Add your S3 bucket domain
+      'pbs.twimg.com',  // Twitter profile images domain
+      'api.twitter.com'  // Twitter profile images domain
     ],
   },
   headers: async () => {
     return process.env.NEXT_PUBLIC_PRODUCTION === 'production'
       ? [] // No additional headers in production
       : [
-          {
-            source: '/:path*',
-            headers: [
-              {
-                key: 'X-Robots-Tag',
-                value: 'noindex',
-              },
-            ],
-          },
-        ];
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'X-Robots-Tag',
+              value: 'noindex',
+            },
+          ],
+        },
+      ];
   },
   webpack: (config, context) => {
     if (config.plugins) {
